27fa9194abd373d30fdfb64c7b4f03a6746b381d
Diana091095 <diana091095@mail.ru>
2024-10-30 12:57:18 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,11 @@
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(x[0])-len(x[1]) or len(x[1])-len(x[0]) for x in zip(first, second) if len(x[0]) != len(x[1]))
+
+
+second_result = (True if len(first[i]) == len(second[i]) else False for i in range(len(first)))
+
+print(list(first_result))
+print(list(second_result))
